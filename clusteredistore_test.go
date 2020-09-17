e0c9ab26a005905ec95900bd98c97230a3756882
Henry Kwan <piengeng@gmail.com>
2020-09-17 19:27:45 +0800

    first import

---
+++ clusteredistore_test.go
@@ -0,0 +1,111 @@
+package clusteredistore
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-redis/redis"
+	"github.com/gorilla/sessions"
+)
+
+var (
+	redisAddr  = "ubuntu.home:6379"
+	redisAddrs = []string{
+		"ubuntu.home:7000", "ubuntu.home:7001", "ubuntu.home:7002",
+		"ubuntu.home:7003", "ubuntu.home:7004", "ubuntu.home:7005",
+	}
+	clustered = true
+	keyPrefix = "a:" // hard coded in redisstore.go
+	client    redis.UniversalClient
+)
+
+func createStore(t *testing.T, keyPrefix string, options sessions.Options) *RedisStore {
+	store := NewRedisStore(client)
+	store.KeyPrefix(keyPrefix)
+	store.SetOptions(options)
+	return store
+}
+func TestSuite_Basic(t *testing.T) {
+	if clustered { // setup
+		client = redis.NewClusterClient(&redis.ClusterOptions{Addrs: redisAddrs})
+	} else {
+		client = redis.NewClient(&redis.Options{Addr: redisAddr})
+	}
+	defer client.Close() // teardown
+
+	t.Run("create store then request then session", func(t *testing.T) {
+		store := createStore(t, keyPrefix, sessions.Options{Path: "/", Domain: "example.com", MaxAge: 60 * 5})
+		request, err := http.NewRequest("GET", "http://www.example.com", nil)
+		if err != nil {
+			t.Fatal("failed to create request", err)
+		}
+		session, err := store.New(request, "hello")
+		if err != nil {
+			t.Fatal("failed to create session", err)
+		}
+		if session.IsNew == false {
+			t.Fatal("session is not new")
+		}
+	})
+
+	t.Run("setting options", func(t *testing.T) {
+		store := createStore(t, keyPrefix, sessions.Options{Path: "/", Domain: "example.com", MaxAge: 60 * 5})
+		opts := sessions.Options{Path: "/path", MaxAge: 99999}
+		store.SetOptions(opts)
+		request, err := http.NewRequest("GET", "http://www.example.com", nil)
+		if err != nil {
+			t.Fatal("failed to create request", err)
+		}
+		session, _ := store.New(request, "hello")
+		if session.Options.Path != opts.Path || session.Options.MaxAge != opts.MaxAge {
+			t.Fatal("failed to set options")
+		}
+	})
+
+	t.Run("saving session", func(t *testing.T) {
+		store := createStore(t, keyPrefix, sessions.Options{Path: "/", Domain: "example.com", MaxAge: 60 * 5})
+		request, err := http.NewRequest("GET", "http://www.example.com", nil)
+		if err != nil {
+			t.Fatal("failed to create request", err)
+		}
+		w := httptest.NewRecorder()
+		session, err := store.New(request, "hello")
+		if err != nil {
+			t.Fatal("failed to create session", err)
+		}
+		session.Values["key"] = "value"
+		err = session.Save(request, w)
+		if err != nil {
+			t.Fatal("failed to save: ", err)
+		}
+	})
+
+	t.Run("deleting session", func(t *testing.T) {
+		store := createStore(t, keyPrefix, sessions.Options{Path: "/", Domain: "example.com", MaxAge: 60 * 5})
+		request, err := http.NewRequest("GET", "http://www.example.com", nil)
+		if err != nil {
+			t.Fatal("failed to create request", err)
+		}
+		w := httptest.NewRecorder()
+		session, err := store.New(request, "hello")
+		if err != nil {
+			t.Fatal("failed to create session", err)
+		}
+		session.Values["username"] = "henry"
+		err = session.Save(request, w)
+		if err != nil {
+			t.Fatal("failed to save session: ", err)
+		}
+		session.Options.MaxAge = -1 // comment this to see un-delete session still exists
+		err = session.Save(request, w)
+		if err != nil {
+			t.Fatal("failed to delete session: ", err)
+		}
+		target := keyPrefix + session.ID
+		// session.Save() doesn't always reflect the key existence, using redis client to check
+		if client.Exists(target).Val() == 1 {
+			t.Fatal("delete target still exists: ", session.ID)
+		}
+	})
+}
---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/piengeng/clusteredistore
+
+go 1.15
+
+require (
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/gorilla/sessions v1.2.1
+	github.com/onsi/ginkgo v1.14.1 // indirect
+	github.com/onsi/gomega v1.10.2 // indirect
+	golang.org/x/net v0.0.0-20200904194848-62affa334b73 // indirect
+	golang.org/x/sys v0.0.0-20200916084744-dbad9cb7cb7a // indirect
+	golang.org/x/text v0.3.3 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
